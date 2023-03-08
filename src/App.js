f1eab470508bc75fb94d8d0f1a012c0171bcdabc
Lucas Mollerstuen <115112679+oz-mollerstuen@users.noreply.github.com>
2023-03-08 15:06:55 -0800

    Fixes import error for firebase

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import firebase from './components/Firebase/firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
 import React from 'react';
 import Sidebar from './components/Sidebar/Index';
 import SignIn from './pages/SignIn';
@@ -16,20 +19,8 @@ import {
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { mainFirebase } from './components/Firebase/firebase';
 import './App.css';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
 
-const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID, 
-};
-
-firebase.initializeApp(config);
+firebase.initializeApp(mainFirebase);
 
 function App() {
   return (
@@ -41,8 +32,8 @@ function App() {
             <React.Fragment>
               <Router>
                 <Routes>
-                  <Route path="/" element={<SignIn firebase={mainFirebase} />} />
-                  <Route path="/sign-up" element={<SignUp firebase={mainFirebase} />} />
+                  <Route path="/" element={<SignIn />} />
+                  <Route path="/sign-up" element={<SignUp />} />
                   <Route path="/dashboard" element={<Dashboard />} />
                   <Route path="/forget-password" element={<PasswordForget />} />
                   <Route path="/sidebar" element={<Sidebar />} />
